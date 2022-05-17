90584ce8409cd11871b3d0223f5c2652f9c35241
Taiwo Jolomi <taiwojolomi15@gmail.com>
2022-05-17 14:10:52 +0100

    changed everything

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+$(function () {
+    $('.hamburger-menu').on('click', function () {
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+    });
 
-// setting contact form variables
+    AOS.init({
+        easing: 'ease',
+        duration: 1000
+    });
+});
+
+// setting form variables
 
 const form = document.querySelector('[data-form]');
 
 const formInputs = document.querySelectorAll('[data-form-input]');
 
 const formBtn = document.querySelector('[data-form-btn]');
+
+
+
 
 
 // add event to all form input field
@@ -28,3 +42,27 @@ for (let i = 0; i < formInputs.length; i++) {
         
     });
 }
+
+
+
+// toggle eye icon for password
+
+const togglePassword = document.querySelector('#togglePassword');
+  
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', () => {
+
+    // Toggle the type attribute using
+    // getAttribure() method
+    const type = password
+        .getAttribute('type') === 'password' ?
+        'text' : 'password';
+            
+    password.setAttribute('type', type);
+
+    // Toggle the eye and bi-eye icon
+    this.classList.toggle('bi-eye');
+});
+
+
