1e55c6ce1120cdbe96694925145802d4dc4515be
Taiwo Jolomi <83990965+JolomiTee@users.noreply.github.com>
2022-05-17 14:30:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ $(function () {
         $('.toggle').toggleClass('open');
         $('.nav-list').toggleClass('open');
     });
-
-    AOS.init({
-        easing: 'ease',
-        duration: 1000
-    });
 });
 
 // setting form variables
